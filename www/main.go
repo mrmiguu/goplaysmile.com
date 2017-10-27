d8fe3d8f539851191ea1c7fbbef3d6747a99817a
PǝRV <alexander.w.matulionis@gmail.com>
2017-10-26 17:47:42 -0700

    Some more work

--- www/main.go
+++ www/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/mrmiguu/dxweb"
 	"github.com/mrmiguu/gps_online/shared"
 	"github.com/mrmiguu/jsutil"
@@ -16,6 +18,7 @@ func init() {
 func main() {
 	bgld := dxweb.LoadImage("assets/bg.png")
 	lgnld := dxweb.LoadImage("assets/login-btn.png")
+	pwdld := dxweb.LoadImage("assets/pass-btn.png")
 	hitld := dxweb.LoadSound("assets/hit.wav")
 	gpsld := dxweb.LoadImage("assets/gps-start.png")
 	splashld := dxweb.LoadImage("assets/splash.png")
@@ -62,44 +65,89 @@ func main() {
 	go typed.Resize(84, 100)
 	typed.Show(true, 100)
 
+	var name string
 	Name := sock.Wstring()
 readKeys:
 	for {
 		select {
 		case <-lgn.Hit:
 			hit.Play()
-			name := typed.Get()
+			name = typed.Get()
 			Name <- name
+
 			go inputbg.Show(false, 100)
 			typed.Show(false, 100)
 			typed.Set("")
-
-			SOCKName := shared.SOCKName(name)
-
-			Found := sock.Rbool(SOCKName)
-			Pass := sock.Wbytes(SOCKName)
-			Err := sock.Rerror(SOCKName)
-
-			found := <-Found
-			go inputbg.Show(true, 100)
-			typed.Show(true, 100)
-			if found {
-
-			} else {
-			}
 
 			_, height = lgn.Size()
 			x, _ = lgn.Pos()
 			go lgn.Move(x, -height/2, 100)
 			lgn.Show(false, 100)
 
-			sock.Close(SOCKName)
 			break readKeys
 
 		case txt := <-input:
 			typed.Set(txt)
 		}
 	}
+
+	SOCKName := shared.SOCKName(name)
+
+	Found := sock.Rbool(SOCKName)
+	found := <-Found
+
+	pwd := <-pwdld
+	_, height = pwd.Size()
+	x, _ = pwd.Pos()
+	pwd.Move(x, -height/2)
+
+	var pass []byte
+	Pass := sock.Wbytes(SOCKName)
+	Err := sock.Rerror(SOCKName)
+
+	go inputbg.Show(true, 100)
+	typed.Show(true, 100)
+
+	go pwd.Move(x, height/2, 100)
+	pwd.Show(true, 100)
+
+readKeys:
+	for {
+		select {
+		case err := <-Err:
+			if err == nil {
+				break readKeys
+			}
+
+		case <-pwd.Hit:
+			hit.Play()
+			Pass <- pass
+
+			go inputbg.Show(false, 100)
+			typed.Show(false, 100)
+			typed.Set("")
+
+			go pwd.Move(x, -height/2, 100)
+			pwd.Show(false, 100)
+
+			go inputbg.Show(true, 100)
+			typed.Show(true, 100)
+
+			go pwd.Move(x, height/2, 100)
+			pwd.Show(true, 100)
+
+		case txt := <-input:
+			L := len(txt)
+			if L == 0 {
+				continue
+			}
+			typed.Set(strings.Repeat("*", L-1) + string(txt[L-1]))
+			pass = []byte(txt)
+		}
+	}
+
+	sock.Close(SOCKName)
+
 	jsutil.CloseKeyboard()
 
 	select {}
