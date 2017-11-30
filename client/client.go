d98d8f1059f2640154b1facdd6c6f5c04be48d5f
PǝRV <alexander.w.matulionis@gmail.com>
2017-11-29 22:50:28 -0800

    Finished gpso store and started bag

--- client/client.go
+++ client/client.go
@@ -292,9 +292,9 @@ func main() {
 	icobar := <-dxweb.LoadImage("assets/icobar.png")
 	icobar.Move(dxweb.Top(icobar))
 
-	// icoBag := <-dxweb.LoadImage("assets/ico-bag.png")
-	// icoGiftshop := <-dxweb.LoadImage("assets/ico-giftshop.png")
-	icoGpsos := <-dxweb.LoadImage("assets/ico-gpsos.png")
+	icoBag := <-icobar.LoadImage("assets/ico-bag.png")
+	icoGpsos := <-icobar.LoadImage("assets/ico-gpsos.png")
+	icoGiftshop := <-icobar.LoadImage("assets/ico-giftshop.png")
 	// icoMarket := <-dxweb.LoadImage("assets/ico-market.png")
 	// icoSearch := <-dxweb.LoadImage("assets/ico-search.png")
 
@@ -305,36 +305,95 @@ func main() {
 
 	dolls := []dxweb.Image{
 		<-dxweb.LoadImage("assets/die/hats/boot.png"),
-		<-dxweb.LoadImage("assets/die/hats/brownshoe.png"),
-		<-dxweb.LoadImage("assets/die/hats/catmask.png"),
-		<-dxweb.LoadImage("assets/die/hats/crosshelm.png"),
+		// <-dxweb.LoadImage("assets/die/hats/brownshoe.png"),
+		// <-dxweb.LoadImage("assets/die/hats/catmask.png"),
+		// <-dxweb.LoadImage("assets/die/hats/crosshelm.png"),
 		<-dxweb.LoadImage("assets/die/hats/crown.png"),
-		<-dxweb.LoadImage("assets/die/hats/dogmask.png"),
+		// <-dxweb.LoadImage("assets/die/hats/dogmask.png"),
 		<-dxweb.LoadImage("assets/die/hats/fryingpan.png"),
 		<-dxweb.LoadImage("assets/die/hats/gradcap.png"),
 		<-dxweb.LoadImage("assets/die/hats/highheel.png"),
-		<-dxweb.LoadImage("assets/die/hats/sandal.png"),
+		// <-dxweb.LoadImage("assets/die/hats/sandal.png"),
 		<-dxweb.LoadImage("assets/die/hats/shortcake.png"),
 		<-dxweb.LoadImage("assets/die/hats/sneaker.png"),
 		<-dxweb.LoadImage("assets/die/hats/sunhat.png"),
 		<-dxweb.LoadImage("assets/die/hats/tophat.png"),
 	}
-	// for i := range dolls {
-	// 	dolls[i].Disable(true)
-	// }
 
 	blank := <-dxweb.LoadImage("assets/blank-scr.png")
+	bagItems := []dxweb.Image{
+		// <-blank.LoadImage("assets/die/hats/boot.png"),
+		<-blank.LoadImage("assets/die/hats/brownshoe.png"),
+		<-blank.LoadImage("assets/die/hats/catmask.png"),
+		<-blank.LoadImage("assets/die/hats/crosshelm.png"),
+		// <-blank.LoadImage("assets/die/hats/crown.png"),
+		<-blank.LoadImage("assets/die/hats/dogmask.png"),
+		// <-blank.LoadImage("assets/die/hats/fryingpan.png"),
+		// <-blank.LoadImage("assets/die/hats/gradcap.png"),
+		// <-blank.LoadImage("assets/die/hats/highheel.png"),
+		<-blank.LoadImage("assets/die/hats/sandal.png"),
+		// <-blank.LoadImage("assets/die/hats/shortcake.png"),
+		// <-blank.LoadImage("assets/die/hats/sneaker.png"),
+		// <-blank.LoadImage("assets/die/hats/sunhat.png"),
+		// <-blank.LoadImage("assets/die/hats/tophat.png"),
+	}
+	scrollverlay := <-blank.LoadImage("assets/scrollverlay.png")
 	tab := <-blank.LoadImage("assets/tab.png")
+
 	cost2 := <-blank.LoadImage("assets/gpsos/cost_2.png")
+	cost4 := <-blank.LoadImage("assets/gpsos/cost_4.png")
+	cost9 := <-blank.LoadImage("assets/gpsos/cost_9.png")
+	cost15 := <-blank.LoadImage("assets/gpsos/cost_15.png")
+	cost25 := <-blank.LoadImage("assets/gpsos/cost_25.png")
+	cost49 := <-blank.LoadImage("assets/gpsos/cost_49.png")
+	cost69 := <-blank.LoadImage("assets/gpsos/cost_69.png")
 	gpsos10 := <-blank.LoadImage("assets/gpsos/gpsos_10.png")
+	gpsos20 := <-blank.LoadImage("assets/gpsos/gpsos_20.png")
+	gpsos50 := <-blank.LoadImage("assets/gpsos/gpsos_50.png")
+	gpsos100 := <-blank.LoadImage("assets/gpsos/gpsos_100.png")
+	gpsos200 := <-blank.LoadImage("assets/gpsos/gpsos_200.png")
+	gpsos500 := <-blank.LoadImage("assets/gpsos/gpsos_500.png")
+	gpsos1000 := <-blank.LoadImage("assets/gpsos/gpsos_1000.png")
+
 	gpsoHit := <-dxweb.LoadSound("assets/gpsos/hit.wav")
+
 	bx, by := blank.Pos()
 	_, bh := blank.Size()
+
 	halfGpso := 58
+
 	c2w, _ := cost2.Size()
-	cost2.Move(c2w/2+halfGpso, 0)
-	g10w, _ := gpsos10.Size()
-	gpsos10.Move(-g10w/2+halfGpso, 0)
+	c4w, _ := cost4.Size()
+	c9w, _ := cost9.Size()
+	c15w, _ := cost15.Size()
+	c25w, _ := cost25.Size()
+	c49w, _ := cost49.Size()
+	c69w, _ := cost69.Size()
+
+	g10w, g10h := gpsos10.Size()
+	g20w, g20h := gpsos20.Size()
+	g50w, g50h := gpsos50.Size()
+	g100w, g100h := gpsos100.Size()
+	g200w, g200h := gpsos200.Size()
+	g500w, g500h := gpsos500.Size()
+	g1000w, g1000h := gpsos1000.Size()
+
+	cost2.Move(c2w/2+halfGpso, -g100h/2-g50h-g20h-g10h/2-30)
+	cost4.Move(c4w/2+halfGpso, -g100h/2-g50h-g20h/2-20)
+	cost9.Move(c9w/2+halfGpso, -g100h/2-g50h/2-10)
+	cost15.Move(c15w/2+halfGpso, 0)
+	cost25.Move(c25w/2+halfGpso, g100h/2+g200h/2+10)
+	cost49.Move(c49w/2+halfGpso, g100h/2+g200h+g500h/2+20)
+	cost69.Move(c69w/2+halfGpso, g100h/2+g200h+g500h+g1000h/2+30)
+
+	gpsos10.Move(-g10w/2+halfGpso, -g100h/2-g50h-g20h-g10h/2-30)
+	gpsos20.Move(-g20w/2+halfGpso, -g100h/2-g50h-g20h/2-20)
+	gpsos50.Move(-g50w/2+halfGpso, -g100h/2-g50h/2-10)
+	gpsos100.Move(-g100w/2+halfGpso, 0)
+	gpsos200.Move(-g200w/2+halfGpso, g100h/2+g200h/2+10)
+	gpsos500.Move(-g500w/2+halfGpso, g100h/2+g200h+g500h/2+20)
+	gpsos1000.Move(-g1000w/2+halfGpso, g100h/2+g200h+g500h+g1000h/2+30)
+
 	_, th := tab.Size()
 	tab.Move(0, bh/2-th/2)
 
@@ -350,24 +409,66 @@ func main() {
 		case <-tab.Hit:
 			hit.Play()
 			blank.Move(bx, -bh/2, 250)
+			tab.Show(false)
+
+			cost2.Show(false)
+			cost4.Show(false)
+			cost9.Show(false)
+			cost15.Show(false)
+			cost25.Show(false)
+			cost49.Show(false)
+			cost69.Show(false)
+			gpsos10.Show(false)
+			gpsos20.Show(false)
+			gpsos50.Show(false)
+			gpsos100.Show(false)
+			gpsos200.Show(false)
+			gpsos500.Show(false)
+			gpsos1000.Show(false)
+
 			blank.Show(false)
 		case <-gpsos10.Hit:
 			gpsoHit.Play()
 			payPal("3AGKVQVLS9WF2")
+		case <-gpsos20.Hit:
+			gpsoHit.Play()
+			payPal("QAZLX4G4DRYXY")
+		case <-gpsos50.Hit:
+			gpsoHit.Play()
+			payPal("T3MQB3N6G3ZPL")
+		case <-gpsos100.Hit:
+			gpsoHit.Play()
+			payPal("3AGKVQVLS9WF2")
+		case <-gpsos200.Hit:
+			gpsoHit.Play()
+			payPal("KDXU5V9TUXUMU")
+		case <-gpsos500.Hit:
+			gpsoHit.Play()
+			payPal("V79FQ6H9M2TW2")
+		case <-gpsos1000.Hit:
+			gpsoHit.Play()
+			payPal("VG8SE9DGAGAFC")
 		}
 	}
 
 	if foundName && foundHash {
 		blank.Show(true)
-		// change so parent's Show changes children
-		// change so parent's Show changes children
-		// change so parent's Show changes children
 		tab.Show(true)
+
 		cost2.Show(true)
+		cost4.Show(true)
+		cost9.Show(true)
+		cost15.Show(true)
+		cost25.Show(true)
+		cost49.Show(true)
+		cost69.Show(true)
 		gpsos10.Show(true)
-		// change so parent's Show changes children
-		// change so parent's Show changes children
-		// change so parent's Show changes children
+		gpsos20.Show(true)
+		gpsos50.Show(true)
+		gpsos100.Show(true)
+		gpsos200.Show(true)
+		gpsos500.Show(true)
+		gpsos1000.Show(true)
 	}
 
 	x, _ := die.Pos()
@@ -388,10 +489,18 @@ func main() {
 	Map := maps[mapnms[rndmap]]
 	Map.Show(true)
 
-	icobar.Show(true)
+	ibw, _ := icoBag.Size()
+	igw, _ := icoGpsos.Size()
+	igsw, _ := icoGiftshop.Size()
+	ihalf := (ibw + igw + igsw) / 2
+	icoBag.Move(-ihalf+ibw/2, 0)
+	icoGpsos.Move(-ihalf+ibw+igw/2, 0)
+	icoGiftshop.Move(-ihalf+ibw+igw+igsw/2, 0)
 
-	icoGpsos.Move(icobar.Pos())
+	icobar.Show(true)
+	icoBag.Show(true)
 	icoGpsos.Show(true)
+	icoGiftshop.Show(true)
 
 	if foundName && foundHash {
 		gpsosUI()
@@ -401,25 +510,69 @@ func main() {
 
 	for {
 		select {
-		case <-icoGpsos.Hit:
+		case <-icoBag.Hit:
 			hit.Play()
 
 			blank.Show(true)
-			// change so parent's Show changes children
-			// change so parent's Show changes children
-			// change so parent's Show changes children
+			_, bi0h := bagItems[0].Size()
+			_, bi1h := bagItems[1].Size()
+			_, bi2h := bagItems[2].Size()
+			_, bi3h := bagItems[3].Size()
+			_, bi4h := bagItems[4].Size()
+			bagItems[0].Move(0, -bi0h/2-bi1h-bi2h/2)
+			bagItems[0].Show(true)
+			bagItems[1].Move(0, -bi1h/2-bi2h/2)
+			bagItems[1].Show(true)
+			bagItems[2].Show(true)
+			bagItems[3].Move(0, bi2h/2+bi3h/2)
+			bagItems[3].Show(true)
+			bagItems[4].Move(0, bi2h/2+bi3h+bi4h/2)
+			bagItems[4].Show(true)
+			scrollverlay.Show(true)
 			tab.Show(true)
-			cost2.Show(true)
-			gpsos10.Show(true)
-			// change so parent's Show changes children
-			// change so parent's Show changes children
-			// change so parent's Show changes children
 
 			blank.Move(bx, by, 250)
-
 			land.Play()
 
+			<-tab.Hit
+			hit.Play()
+			blank.Move(bx, -bh/2, 250)
+			bagItems[0].Show(false)
+			bagItems[1].Show(false)
+			bagItems[2].Show(false)
+			bagItems[3].Show(false)
+			bagItems[4].Show(false)
+			scrollverlay.Show(false)
+			blank.Show(false)
+			continue
+
+		case <-icoGpsos.Hit:
+			hit.Play()
+			blank.Show(true)
+			tab.Show(true)
+
+			cost2.Show(true)
+			cost4.Show(true)
+			cost9.Show(true)
+			cost15.Show(true)
+			cost25.Show(true)
+			cost49.Show(true)
+			cost69.Show(true)
+			gpsos10.Show(true)
+			gpsos20.Show(true)
+			gpsos50.Show(true)
+			gpsos100.Show(true)
+			gpsos200.Show(true)
+			gpsos500.Show(true)
+			gpsos1000.Show(true)
+
+			blank.Move(bx, by, 250)
+			land.Play()
 			gpsosUI()
+			continue
+
+		case <-icoGiftshop.Hit:
+			hit.Play()
 			continue
 
 		case <-paperdoll.Hit:
